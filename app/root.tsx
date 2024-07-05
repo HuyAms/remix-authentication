54af1d1f1a1ab69e552d62d7e761520c098ef7d5
Huy Trinh <htrinh@alpha-sense.com>
2024-07-05 13:27:17 +0300

    feat: init MUI

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Links,
   Meta,
@@ -5,9 +6,29 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import { withEmotionCache } from '@emotion/react';
+import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/material';
+import ClientStyleContext from './ClientStyleContext';
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const Layout = withEmotionCache(({ children }: {children: React.ReactNode}, emotionCache) => {
+  const clientStyleData = React.useContext(ClientStyleContext);
+
+  // Only executed on client
+  useEnhancedEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      // eslint-disable-next-line no-underscore-dangle, @typescript-eslint/no-explicit-any
+      (emotionCache.sheet as any)._insertTag(tag);
+    });
+    // reset cache to reapply global styles
+    clientStyleData.reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <html lang="en">
       <head>
@@ -15,6 +36,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
+        <meta name="emotion-insertion-point" content="emotion-insertion-point" />
       </head>
       <body>
         {children}
@@ -23,7 +49,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </body>
     </html>
   );
-}
+})
 
 export default function App() {
   return <Outlet />;
